d5281abba76e8e7b7e913b80151029a034832745
William Hayward <haywardwilliam1996@gmail.com>
2018-02-01 17:20:54 +1000

    +Added basic WebSocket server to Lipwig

--- src/Lipwig.ts
+++ src/Lipwig.ts
@@ -1,13 +1,33 @@
 /**
  * @author: William Hayward
  */
-import {server as WebSocket} from 'websocket';
+import * as http from 'http';
+import * as WebSocket from 'websocket';
 import {Room} from './Room';
 import {User} from './User';
 export class Lipwig {
-    private ws: WebSocket;
-    constructor() {
-        this.ws = new WebSocket();
+    private ws: WebSocket.server;
+    constructor(port: number = 8080) {
+        const server: http.Server = http.createServer();
+        server.listen(port, () => {
+            console.log('Listening on ' + port);
+        });
+
+        this.ws = new WebSocket.server({
+            httpServer: server,
+            autoAcceptConnections: false
+        });
+
+        this.ws.on('request', this.newRequest);
+    }
+
+    private newRequest(request: WebSocket.request): void {
+
+    }
+
+    private isOriginAllowed(origin: string): boolean {
+        // TODO: Origin checking
+        return true;
     }
 
     private newConnection(): void {
