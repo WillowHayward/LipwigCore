6aa462510f616bc78abfb6549f0bef9cdf9e4c2d
William Hayward <haywardwilliam1996@gmail.com>
2018-02-06 16:02:53 +1000

    +Added Utility class (currently just random string generation) -Removed tslint secure random rule

---
+++ src/Utility.ts
@@ -0,0 +1,20 @@
+/**
+ * @author: William Hayward
+ */
+const alphabet: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+export namespace Utility {
+    const generateString: Function = (length: number = 4, characters: string = alphabet): string => {
+        if (characters.length === 0) {
+            return '';
+        }
+        let str: string = '';
+        let index: number;
+        while (str.length < length) {
+            index = Math.random() * characters.length;
+            index = Math.floor(index);
+            str += characters[index];
+        }
+
+        return str;
+    };
+}
