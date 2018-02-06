a85f11fc6790e511fa0548636dd823e52794c115
William Hayward <haywardwilliam1996@gmail.com>
2018-02-06 20:37:57 +1000

    -Removed uglifying from build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,20 +3,6 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            build: {
-                files: [{
-                    expand: true,
-                    src: ['dist/*.js', '!dist/*.min.js'],
-                    dest: './',
-                    cwd: '.',
-                    rename: function (dst, src) {
-                        // To keep the source js files and make new files as `*.min.js`:
-                        return dst + '/' + src.replace('.js', '.min.js');
-                    }
-                }]
-            }
-        },
         exec: {
             tsc: './node_modules/typescript/bin/tsc',
             mocha: './node_modules/mocha/bin/mocha'
@@ -49,6 +35,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s).
-    grunt.registerTask('build', ['tslint', 'exec',  'uglify'])
+    grunt.registerTask('build', ['tslint', 'exec'])
     grunt.registerTask('default', ['build']);
   };
