c42241f7d19c3616a8a180b943171d14617fe436
William Hayward <haywardwilliam1996@gmail.com>
2018-02-06 14:45:03 +1000

    + EventManager now triggers once and on in order they were added + Allows for arguments to passed in emit() + More complex type definitions

--- src/EventManager.ts
+++ src/EventManager.ts
@@ -1,48 +1,55 @@
 /**
  * @author: William Hayward
  */
-type EmptyFunction = () => void;
+// tslint:disable:no-any
+// "any" required for public API arguments
+type Function = (...args: any[]) => void;
+type Callback = {
+    once: boolean;
+    fn: Function;
+    context: object;
+};
 type FunctionMap = {
-    [index: string]: EmptyFunction[];
+    [index: string]: Callback[];
 };
 export class EventManager {
     private events: FunctionMap;
-    private singleEvents: FunctionMap;
-    // TODO: Merge these down to one map to maintain the event listener order
+
     constructor() {
         this.events = {};
-        this.singleEvents = {};
     }
 
-    public on(event: string, fn: EmptyFunction, context: object = this): void {
+    public on(event: string, fn: Function, context: object = this): void {
+        this.addEvent(event, fn, context, false);
+    }
+
+    public once(event: string, fn: Function, context: object = this): void {
+        this.addEvent(event, fn, context, true);
+    }
+
+    public emit(event: string, ...args: any[]): void {
+        const callbacks: Callback[] = this.events[event];
+        if (callbacks === undefined) {
+            return;
+        }
+
+        callbacks.forEach((callback: Callback, index: number, object: Callback[]): void => {
+            callback.fn.apply(callback.context, args);
+            if (callback.once) {
+                object.splice(index, 1);
+            }
+        });
+    }
+
+    private addEvent(event: string, fn: Function, context: object, once: boolean): void {
         if (this.events[event] === undefined) {
             this.events[event] = [];
         }
-        this.events[event].push(fn.bind(context));
-    }
-
-    public once(event: string, fn: EmptyFunction, context: object = this): void {
-        if (this.singleEvents[event] === undefined) {
-            this.singleEvents[event] = [];
-        }
-        this.singleEvents[event].push(fn.bind(context));
-    }
-
-    public emit(event: string): void {
-        const functions: EmptyFunction[] = this.events[event];
-        if (functions !== undefined) {
-            for (const fn of functions) {
-                fn();
-            }
-        }
-
-        const singleFunctions: EmptyFunction[] = this.singleEvents[event];
-        this.singleEvents[event] = [];
-        if (singleFunctions !== undefined) {
-            for (const fn of singleFunctions) {
-                fn();
-            }
-        }
-
+        const callback: Callback = {
+            once: once,
+            fn: fn.bind(context),
+            context: context
+        };
+        this.events[event].push(callback);
     }
 }
