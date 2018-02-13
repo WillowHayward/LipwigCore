040257a644a49c984a7b285d45a2670c5b01a7d2
William Hayward <haywardwilliam1996@gmail.com>
2018-02-13 19:48:58 +1000

    +Added basic stub tests

---
+++ test/Stub.js
@@ -0,0 +1,67 @@
+const Lipwig = require('../lib/Lipwig.js');
+const Stub = require('../lib/Stub.js').Stub;
+let lw;
+describe('Stub', function() {
+    before(function() {
+        lw = new Lipwig();
+    });
+
+    after(function() {
+        lw.exit();
+    });
+
+    it('should be able to connect', function(done) {
+        const user = new Stub('ws://localhost:8080');
+        user.on('connected', function() {
+            done();
+        });
+    });
+
+    it('should be able to create rooms', function(done) {
+        const host = new Stub('ws://localhost:8080');
+        host.on('connected', function() {
+            const message = {
+                event: 'create',
+                data: [],
+                sender: '',
+                recipient: []
+            };
+
+            host.send(message);
+        });
+
+        host.on('created', function(code) {
+            done();
+        });
+    });
+
+    it('should be able to join rooms', function(done) {
+        const host = new Stub('ws://localhost:8080');
+        host.on('connected', function() {
+            const message = {
+                event: 'create',
+                data: [],
+                sender: '',
+                recipient: []
+            };
+
+            host.send(message);
+        });
+
+        host.on('created', function(code) {
+            const client = new Stub('ws://localhost:8080');
+            client.on('connected', function() {
+                const message = {
+                    event: 'join',
+                    data: [code],
+                    sender: '',
+                    recipient: []
+                };
+                client.send(message);
+                client.on('joined', function() {
+                    done();
+                });
+            });
+        });
+    });
+});
