f1c2abb09e1270c42436c8c9e98bde4aabcc3b95
William Hayward <haywardwilliam1996@gmail.com>
2018-02-13 20:37:43 +1000

    +Added kicking tests

---
+++ test/User.js
@@ -0,0 +1,66 @@
+const Lipwig = require('../lib/Lipwig.js');
+const Stub = require('../lib/Stub.js').Stub;
+
+describe('User', function() {
+    before(function() {
+        lw = new Lipwig();
+    });
+
+    after(function() {
+        lw.exit();
+    });
+    function create() {
+        const host = new Stub('ws://localhost:8080');
+        host.on('connected', function() {
+            const message = {
+                event: 'create',
+                data: [],
+                sender: '',
+                recipient: []
+            };
+
+            host.send(message);
+        });
+
+        return host;
+    }
+
+    function join(code) {
+        const client = new Stub('ws://localhost:8080');
+        client.on('connected', function() {
+            const message = {
+                event: 'join',
+                data: [code],
+                sender: '',
+                recipient: []
+            };
+            client.send(message);
+        });
+
+        return client;
+    }
+
+    describe('kick', function() {
+        it('should kick users', function(done) {
+            host = create();
+            host.on('created', function(code) {
+                host.id = code;
+                client = join(code);
+
+                client.on('kicked', function() {
+                    done();
+                });
+            });
+
+            host.on('joined', function(id) {
+                const message = {
+                    event: 'kick',
+                    data: [id, ''],
+                    sender: host.id,
+                    recipient: [id]
+                }
+                host.send(message);
+            });
+        });
+    })
+})
