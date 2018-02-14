250abe5df4bea29214d122575d555824657d63dc
William Hayward <haywardwilliam1996@gmail.com>
2018-02-14 14:46:38 +1000

    +Added Error tests

--- test/Errors.js
+++ test/Errors.js
@@ -14,12 +14,71 @@ describe('Errors', function() {
     after(function() {
         lw.exit();
     });
+
+    function create() {
+        const host = new Stub('ws://localhost:8080');
+        host.on('connected', function() {
+            const message = {
+                event: 'create',
+                data: [],
+                sender: '',
+                recipient: []
+            };
+
+            host.send(message);
+        });
+
+        return host;
+    }
+
+    function join(code) {
+        const client = new Stub('ws://localhost:8080');
+        client.on('connected', function() {
+            const message = {
+                event: 'join',
+                data: [code],
+                sender: '',
+                recipient: []
+            };
+            client.send(message);
+        });
+
+        return client;
+    }
     
     describe(ErrorCode.MALFORMED + ' - Malformed', function() {
-
+        it('should catch completely malformed messages', function(done) {
+            const message = 'sdsdsd88wdwe';
+            const host = new Stub('ws://localhost:8080');
+            host.on('connected', function() {
+                host.send(message);
+            });
+            host.on('error', function(code) {
+                if (code === ErrorCode.MALFORMED) {
+                    done();
+                }
+            });
+        });
     });
     describe(ErrorCode.ROOMNOTFOUND + ' - Room not Found', function() {
+        it('should throw an error when trying to join a non-existent room', function(done) {
+            const client = new Stub('ws://localhost:8080')
+            client.on('connected', function() {
+                const message = {
+                    event: 'join',
+                    data: ['ABCD'],
+                    sender: '',
+                    recipient: []
+                };
+                client.send(message);
+            });
 
+            client.on('error', function(code) {
+                if (code === ErrorCode.ROOMNOTFOUND) {
+                    done();
+                }
+            })
+        });
     });
     describe(ErrorCode.ROOMFULL + ' - Room Full', function() {
 
@@ -28,6 +87,39 @@ describe('Errors', function() {
 
     });
     describe(ErrorCode.INSUFFICIENTPERMISSIONS + ' - Insufficient Permission', function() {
+        it('should not allow users to kick other users', function(done) {
+            const host = new Stub('ws://localhost:8080');
+            host.on('connected', function() {
+                let message = {
+                    event: 'create',
+                    data: [],
+                    sender: '',
+                    recipient: []
+                }
+                host.send(message);
+            });
 
+            host.on('created', function(code) {
+                user1 = join(code);
+                user2 = join(code);
+
+                user2.on('joined', function(id) {
+                    message = {
+                        event: 'kick',
+                        data: [id, 'testing'],
+                        sender: code,
+                        recipient: [id]
+                    };
+
+                    user1.send(message);
+                });
+
+                user1.on('error', function(code) {
+                    if (code === ErrorCode.INSUFFICIENTPERMISSIONS) {
+                        done();
+                    }
+                })
+            });
+        });
     });
 });
