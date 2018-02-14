b92d45e6928fdc83c61cf43b5dc2c3836c094a32
William Hayward <haywardwilliam1996@gmail.com>
2018-02-14 14:46:26 +1000

    +Added offline queue to stub

--- src/Stub.ts
+++ src/Stub.ts
@@ -8,10 +8,14 @@ import { Message } from './Types';
 
 export class Stub extends EventManager {
     private socket: WebSocket.connection;
+    private queue: Message[];
     constructor(url: string) {
         super();
         const client: WebSocket.client = new WebSocket.client();
         client.connect(url);
+
+        this.socket = null;
+        this.queue = [];
 
         client.on('connect', (connection: WebSocket.connection): void => {
             this.socket = connection;
@@ -19,10 +23,19 @@ export class Stub extends EventManager {
                 this.handle(evt);
             });
             this.emit('connected');
+
+            this.queue.forEach((message: Message): void => {
+                this.send(message);
+            });
         });
     }
 
     public send(message: Message): void {
+        if (this.socket === null) {
+            this.queue.push(message);
+
+            return;
+        }
         const text: string = JSON.stringify(message);
         this.socket.send(text);
     }
