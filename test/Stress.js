23d95f166c2ed97dd849b31e508cb02902d551ba
William Hayward <haywardwilliam1996@gmail.com>
2018-02-19 04:09:51 +1000

    +Made stress tests dynamic (closes #8)

--- test/Stress.js
+++ test/Stress.js
@@ -6,17 +6,8 @@ describe('Stress', function() {
     this.timeout(0);
     let lw;
     let time;
-    function estimatedTime(s) {
-        var ms = s % 1000;
-        s = (s - ms) / 1000;
-        var secs = s % 60;
-        s = (s - secs) / 60;
-        var mins = s % 60;
-        var hrs = (s - mins) / 60;
-      
-        return hrs + ' hours, ' + mins + ' minutes, and ' + secs + ' seconds.';
-    }
-    beforeEach(function(done) {
+
+    before(function(done) {
         console.log('Test starting at: ' + new Date());
         lw = new Lipwig();
         lw.on('started', function() {
@@ -24,16 +15,15 @@ describe('Stress', function() {
         });
     });
     
-    afterEach(function(done) {
+    after(function(done) {
         this.timeout(0);
         lw.exit();
         lw.on('closed', done);
     });
 
+
     function create(options) {
-        if (options === undefined) {
-            options = {};
-        }
+        options = options || {};
 
         const host = new Stub('ws://localhost:8080');
         host.on('connected', function() {
@@ -68,206 +58,190 @@ describe('Stress', function() {
         return client;
     }
 
-    it('should allow for hundreds of messages to be passed between host and client', function(done) {
-        const start = new Date().getTime();
-        const host = create();
-        const total = 1000;
-        host.on('created', function(code) {
-            host.id = code;
-            const client = join(code);
-            const message = {
-                event: 'ping',
-                data: [0],
-                sender: '',
-                recipient: []
-            };
-            client.on('joined', function(id) {
-                message.sender = id;
-                client.send(message);
-            });
+    report = false;
 
-            client.on('pong', function(count) {
-                if ((count / total * 100) % 10 === 0) {
-                    const percentage = count / total * 100;
-                    console.log(percentage + "% completed");
+    function createRooms(remaining, done, codes) {
+        codes = codes || [];
+        host = create();
+        host.on('created', function(code) {
+            codes.push(code);
+
+            this.message = {
+                event: 'pong',
+                data: [],
+                sender: code,
+                recipient: []
+            }
+
+            remaining--;
+            if (remaining) {
+                createRooms(remaining, done, codes);
+            } else {
+                if (report) {
+                    console.log('Rooms created');
                 }
-                if (count >= 1000) {
-                    time = new Date().getTime() - start;
-                    done();
-                } else {
-                    message.data = [count];
-                    client.send(message);
-                }
-            })
+                done(codes);
+            }
         });
 
         host.on('ping', function(count, message) {
-            count++;
-            const pong = {
-                event: 'pong',
-                data: [count],
-                sender: host.id,
-                recipient: [message.sender]
-            };
-            host.send(pong);
+            this.message.data[0] = count;
+            this.message.recipient[0] = message.sender;
+            this.send(this.message);
         });
-    });
+    }
 
-    it('should allow for hundreds of messages to be passed between host and client across hundreds of rooms', function(done) {
-        const start = new Date().getTime();
-        const estimated = time * 1000;
-        const estimatedString = estimatedTime(estimated);
-        console.log('Buckle up, this is estimated to take ' + estimatedString);
-        completedCount = 0;
-        completed = [];
-        codes = [];
-        created = 0;
-        const total = 1000 * 1000
-        let totalComplete = 0;
-        let joined = 0;
-        function createServer(i) {
-            completed[i] = false;
-            host = create();
-
-            host.on('created', function(code) {
-                codes[created] = code;
-                created++;
-                if (created < 1000) {
-                    createServer(i + 1);
-                } else {
-                    console.log('All rooms created');
-                    createClient(0);
+    function joinRooms(remaining, codes, done, users) {
+        users = users || [];
+        code = codes[remaining % codes.length];
+        client = join(code);
+        client.on('joined', function(id) {
+            this.id = id;
+            users.push(this);
+            remaining--;
+            if (remaining) {
+                joinRooms(remaining, codes, done, users);
+            } else {
+                if (report) {
+                    console.log('Rooms joined');
                 }
-            });
+                done(users);
+            }
+        });
 
-            host.on('ping', function(count, id, message) {
-                count++;
-                const pong = {
-                    event: 'pong',
-                    data: [count, id],
-                    sender: message.sender.slice(0, 4),
-                    recipient: [message.sender]
-                };
-                this.send(pong);
-            });
-        }
-
-        function createClient(i) {
-            client = join(codes[i]);
-            client.message = {
-                event: 'ping',
-                data: [0, i],
-                sender: '',
-                recipient: []
-            };
-            client.on('joined', function(id) {
-                joined++;
-                this.message.sender = id;
+        client.on('pong', function(count) {
+            count++;
+            if (count === this.total) {
+                this.emit('finished');
+            } else {
+                this.message.data[0] = count;
                 this.send(this.message);
-                if (joined < 1000) {
-                    createClient(joined);
-                } else {
-                    console.log('All clients joined');
-                }
-            });
+            }
+        });
+    }
 
-            client.on('pong', function(count, index) {
-                totalComplete++;
-                if ((totalComplete / total * 100) % 10 === 0) {
-                    const percentage = totalComplete / total * 100;
-                    console.log(percentage + "% completed");
-                }
-                if (count >= 1000) {
-                    completed[index] = true;
-                    if (completed.indexOf(false) === -1 && completed.length === 1000) {
-                        time = new Date().getTime() - start;
-                        done();
-                    }
-                } else {
-                    this.message.data = [count, index];
-                    this.send(this.message);
-                }
-            })
-        };
-
-        createServer(0);
-    });
-
-    it('should allow for hundreds of messages to be passed between host and client across hundreds of full rooms', function(done) {
-        const estimated = time * 8;
-        const estimatedString = estimatedTime(estimated);
-        console.log('This one is estimated to take ' + estimatedString);
-        completed = [];
+    function stress(rooms, users, messages, done) {
+        expected = rooms * users * messages;
         codes = [];
-        created = 0;
-        joined = 0;
-        const total = 1000 * 1000 * 8;
-        let totalComplete = 0;
-        function createServer(i) {
-            host = create();
-            host.on('created', function(code) {
-                codes[created] = code;
-                created++;
-                if (created < 1000) {
-                    createServer(created);
-                } else {
-                    console.log('All rooms created');
-                    createClient(0);
-                }
-            });
-
-            host.on('ping', function(count, id, message) {
-                count++;
-                const pong = {
-                    event: 'pong',
-                    data: [count, id],
-                    sender: message.sender.slice(0, 4),
-                    recipient: [message.sender]
-                };
-                this.send(pong);
-            });
-        }
-
-        function createClient(i) {
-            completed[i] = [];
-            client = join(codes[i % 1000]);
-            client.message = {
-                event: 'ping',
-                data: [0, i],
-                sender: '',
-                recipient: []
-            };
-            client.on('joined', function(id) {
-                joined++;
-                this.message.sender = id;
-                this.send(this.message);
-                if (joined < 8000) {
-                    createClient(joined);
-                } else {
-                    console.log('All clients joined');
-                }
-            });
-
-            client.on('pong', function(count, index) {
-                totalComplete++;
-                if ((totalComplete / total * 100) % 10 === 0) {
-                    const percentage = totalComplete / total * 100;
-                    console.log(percentage + "% completed");
-                }
-
-                if (count >= 1000) {
-                    completed[index] = true;
-                    if (completed.indexOf(false) === -1 && completed.length === 8000) {
-                        done();
+        completed = [];
+        createRooms(rooms, function(codes) {
+            joinRooms(users * rooms, codes, function(clients) {
+                clients.forEach(function(client, index) {
+                    completed[index] = false;
+                    client.message = {
+                        event: 'ping',
+                        data: [0],
+                        sender: client.id,
+                        recipient: []
                     }
-                } else {
-                    this.message.data = [count, index];
-                    this.send(this.message);
-                }
-            })
-        };
 
-        createServer(0);
+                    client.total = messages;
+
+                    client.on('finished', function() {
+                        completed[index] = true;
+                        if (completed.indexOf(false) === -1 && completed.length === users * rooms) {
+                            done();
+                        }
+                    });
+
+                    client.send(client.message);
+                });
+            });
+        });
+
+    }
+
+    tests = {
+        small: [
+            {
+                rooms: 1,
+                users: 1,
+                messages: 10
+            },
+            {
+                rooms: 10,
+                users: 1,
+                messages: 10
+            },
+            {
+                rooms: 1,
+                users: 8,
+                messages: 10
+            },
+            {
+                rooms: 10,
+                users: 8,
+                messages: 10
+            }
+        ],
+        medium: [
+            {
+                rooms: 1,
+                users: 1,
+                messages: 100
+            },
+            {
+                rooms: 100,
+                users: 1,
+                messages: 100
+            },
+            {
+                rooms: 1,
+                users: 8,
+                messages: 100
+            },
+            {
+                rooms: 100,
+                users: 8,
+                messages: 100
+            }
+        ],
+        large: [
+            {
+                rooms: 1,
+                users: 1,
+                messages: 1000
+            },
+            {
+                rooms: 1000,
+                users: 1,
+                messages: 1000
+            },
+            {
+                rooms: 1,
+                users: 8,
+                messages: 1000
+            },
+            {
+                rooms: 1000,
+                users: 8,
+                messages: 1000
+            }
+        ]
+    }
+    const keys = Object.keys(tests);
+    keys.forEach(function(key) {
+        describe(key, function() {
+            tests[key].forEach(function(test) {
+                rooms = test.rooms;
+                users = test.users;
+                messages = test.messages;
+                total = rooms * users * messages;
+                it('should handle ' + rooms + ' rooms with ' + users + ' users sending ' 
+                    + messages + ' messages each (' + total + ' messages)', function(done) {
+
+                        rooms = test.rooms;
+                        users = test.users;
+                        messages = test.messages;
+                        total = rooms * users * messages;
+                        if (total > 1000000) {
+                            console.log('You might want to get a cup of coffee...');
+                            report = true;
+                        }
+                        stress(rooms, users, messages, done);
+                });
+            });
+        });
     });
 });
 
