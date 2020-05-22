1bead4c7ce4d588537053bf9f6ab7925c5d9bf5b
William Hayward <WillHayCode@gmail.com>
2020-05-22 20:17:28 +1000

    +Moved to typescript eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,12 @@
 module.exports = function(grunt) {
-
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         exec: {
             tsc: './node_modules/typescript/bin/tsc',
             mocha: './node_modules/mocha/bin/mocha --grep Stress --invert',
-            stress: './node_modules/mocha/bin/mocha --grep Stress'
-        },
-        tslint: {
-            options: {
-                rulesDirectory: 'node_modules/tslint-microsoft-contrib',
-                configuration: grunt.file.readJSON("tslint.json")
-            },
-            files: {
-                src: ['src/*.ts']
-            }
+            stress: './node_modules/mocha/bin/mocha --grep Stress',
+            lint: 'yarn eslint . --ext .ts'
         },
         clean: {
             build: ['lib']
@@ -27,18 +18,16 @@ module.exports = function(grunt) {
             }
         }
     });
-    
-    // Load the plugin that provides the "uglify" task.
+
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-exec');
-    grunt.loadNpmTasks('grunt-tslint');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s).
-    grunt.registerTask('build', ['tslint', 'exec:tsc', 'exec:mocha']);
-    grunt.registerTask('stress', ['tslint', 'exec:tsc', 'exec:stress']);
+    grunt.registerTask('build', ['exec:lint', 'exec:tsc', 'exec:mocha']);
+    grunt.registerTask('stress', ['exec:lint', 'exec:tsc', 'exec:stress']);
     grunt.registerTask('quick', ['exec:tsc']);
-    grunt.registerTask('full', ['tslint', 'exec']);
+    grunt.registerTask('full', ['exec:lint', 'exec']);
     grunt.registerTask('default', ['build']);
   };
